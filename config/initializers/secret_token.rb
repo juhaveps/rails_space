5e0299e2a427b63735cd622ba4b153d5d487460a
Juha Vepsalainen <juhaveps@gmail.com>
2011-05-05 15:25:55 +0300

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSpace::Application.config.secret_token = '9ebeb21c2b776fcf8bd7dfb184c473ebeb0e4cfb260200e5f72f8799c7840b00d8b66ec010eeae819a96bfa5103f7372a91b28b9701133b4b0da9e2c468d2a65'
